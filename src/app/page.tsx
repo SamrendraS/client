c260e37590930fb2104099d22a404ad340363163
akiraonstarknet <zkdice@proton.me>
2024-11-07 20:49:10 +0700

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,56 @@
 import styles from './page.module.css';
-import karnotPng from '@public/karnot.png';
+import logoPng from '@public/logo.png';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div>
-          <h1 style={{ marginBottom: 10 }}>Endur</h1>
-          <p>Liquid Staked STRK</p>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            verticalAlign: 'middle',
+            gap: '10px',
+          }}
+        >
+          <img src={logoPng.src} alt="Endur" width={'63px'} />
+          {/* <div>
+          <h1 style={{ marginBottom: 10, fontSize: '50px'}}>Endur</h1>
+          <p>xSTRK {'->'} Liquid Staked STRK</p>
+        </div> */}
+          <p style={{ opacity: 0.7, fontSize: '22px', fontWeight: '600' }}>
+            Coming soon
+          </p>
         </div>
-        <p style={{ opacity: 0.4 }}>Launching soon</p>
-        <div style={{ marginTop: '150px' }}>
-          <p style={{ opacity: 0.4 }}>From the buidlers of</p>
+        {/* <div style={{ marginTop: '100px' }}>
+          <p style={{ opacity: 0.6 }}>From the buidlers of</p>
           <div
             style={{
               display: 'flex',
               verticalAlign: 'middle',
-              gap: '10px',
+              gap: '30px',
               marginTop: '20px',
             }}
           >
             <a href="https://karnot.xyz" target="_blank">
-              <img src={karnotPng.src} alt="Karnot" height={'40px'} />
+              <img src={karnotPng.src} alt="Karnot" height={'30px'} />
             </a>
             <a href="https://strkfarm.xyz" target="_blank">
-              <img
-                src="https://www.strkfarm.xyz/full-logo.svg"
-                height={'34px'}
-                style={{ marginTop: '3px' }}
-                alt="STRKFarm"
-              />
+              <img src={strkfarmPng.src} alt="STRKFarm" height={'35px'} />
             </a>
           </div>
-        </div>
+        </div> */}
+        {/* <div 
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '15px'
+          }}
+        >
+          <a href='https://t.me/+jWY71PfbMMIwMTBl'><img src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/telegram-white-icon.png' width={'50px'}/></a>
+        </div> */}
       </main>
     </div>
   );
