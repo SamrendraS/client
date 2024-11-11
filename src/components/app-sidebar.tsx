67ecfa2b48734497be38274fdd8505590dc37923
akiraonstarknet <zkdice@proton.me>
2024-11-11 19:24:56 +0700

    fix build

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
-import { Fira_Sans } from 'next/font/google';
-import Image from 'next/image';
+import { Fira_Sans } from "next/font/google";
+import Image from "next/image";
 
 import {
   Sidebar,
@@ -7,14 +7,14 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
-} from '@/components/ui/sidebar';
-import { cn } from '@/lib/utils';
-import { ChevronDown } from 'lucide-react';
-import { Icons } from './Icons';
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+import { Icons } from "./Icons";
 
 const font = Fira_Sans({
-  subsets: ['latin'],
-  weight: '400',
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export function AppSidebar() {
@@ -22,34 +22,34 @@ export function AppSidebar() {
     <Sidebar
       className={cn(
         font.className,
-        'h-[calc(100vh-40px)] my-auto mx-5 border border-[#AACBC480] rounded-md rounded-r-sm',
+        "mx-5 my-auto h-[calc(100vh-40px)] rounded-md rounded-r-sm border border-[#AACBC480]",
       )}
     >
-      <SidebarHeader className="bg-[#AACBC433] flex items-center justify-center py-10">
-        <Image src={'/logo.png'} alt="Endur" width={63} height={63} />
+      <SidebarHeader className="flex items-center justify-center bg-[#AACBC433] py-10">
+        <Image src={"/logo.png"} alt="Endur" width={63} height={63} />
       </SidebarHeader>
       <SidebarContent className="bg-[#AACBC433] px-4 pt-5">
-        <SidebarGroup className="text-[#03624C] text-xl cursor-pointer font-semibold transition-all hover:bg-[#17876D] hover:text-white rounded-md">
+        <SidebarGroup className="cursor-pointer rounded-md text-xl font-semibold text-[#03624C] transition-all hover:bg-[#17876D] hover:text-white">
           Dashboard
         </SidebarGroup>
-        <SidebarGroup className="text-xl cursor-pointer font-semibold transition-all bg-[#17876D] text-white rounded-md">
+        <SidebarGroup className="cursor-pointer rounded-md bg-[#17876D] text-xl font-semibold text-white transition-all">
           Staking
         </SidebarGroup>
-        <SidebarGroup className="cursor-pointer transition-all hover:bg-[#17876D] hover:text-white rounded-md pointer-events-none flex justify-between flex-row items-center gap-2">
-          <p className="text-[#03624C] text-xl font-semibold">
+        <SidebarGroup className="pointer-events-none flex cursor-pointer flex-row items-center justify-between gap-2 rounded-md transition-all hover:bg-[#17876D] hover:text-white">
+          <p className="text-xl font-semibold text-[#03624C]">
             Defi <span className="text-sm font-thin">(coming soon)</span>
           </p>
           <ChevronDown className="size-4 text-[#03624C]" />
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter className="bg-[#AACBC433] px-0">
-        <div className="flex items-center gap-4 px-4 mb-5">
+        <div className="mb-5 flex items-center gap-4 px-4">
           <Icons.twitter className="cursor-pointer" />
           <Icons.discord className="cursor-pointer" />
           <Icons.telegram className="cursor-pointer" />
           <Icons.doc className="cursor-pointer" />
         </div>
-        <button className="py-3 px-4 border-t border-[#075A5A1A] flex items-center gap-3 text-[#03624C] text-xl font-medium">
+        <button className="flex items-center gap-3 border-t border-[#075A5A1A] px-4 py-3 text-xl font-medium text-[#03624C]">
           <Icons.chat /> Support
         </button>
       </SidebarFooter>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { num } from 'starknet';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { num } from "starknet";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
