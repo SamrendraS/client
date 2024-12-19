e885a7b02d5d0e46f058689fa1b1e31a35b950a7
akiraonstarknet <zkdice@proton.me>
2024-12-19 18:18:46 +0530

    add rebase script

---
+++ scripts/sync-to-main.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+confirm_action() {
+    read -p "Do you want to process the branch $1? (y/N): " confirm
+    if [[ $confirm == "y" ]]; then
+        return 0
+    else
+        echo "Skipping $1."
+        return 1
+    fi
+}
+
+confirm_main_rebase() {
+    read -p "Do you want to fetch and rebase main to remote? (y/N): " confirm
+    confirm=${confirm,,} # Convert to lowercase
+    if [[ $confirm == "y" ]]; then
+        return 0
+    else
+        echo "Skipping $1."
+        return 1
+    fi
+}
+
+process_branch() {
+    branch=$1
+    if confirm_action "$branch"; then
+        git checkout "$branch" || exit 1
+        git reset --hard main || exit 1
+        git push origin --force "$branch" || exit 1
+        echo "Successfully processed $branch."
+    fi
+}
+
+if confirm_main_rebase; then
+    git checkout main
+    git fetch
+    git rebase origin/main
+    echo "Successfully rebased main to remote."
+fi
+
+# Process branches
+git checkout main
+git fetch
+git rebase origin/main
+process_branch "staging-main"
+process_branch "testnet"
